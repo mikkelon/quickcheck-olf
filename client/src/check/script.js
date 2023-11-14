54d08ba3d69191e801a3a7cd45fdf2287e059b06
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-14 14:05:20 +0100

    implementeret kobling til server api

--- client/src/check/script.js
+++ client/src/check/script.js
@@ -1,8 +1,34 @@
-const cards = document.querySelectorAll(".card");
+const url = "http://localhost:6969/classes";
+const options = {
+  method: "GET",
+};
 
-cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    const classColor = card.getAttribute("data-class");
-    window.location.href = `class/?class=${classColor}`;
+const getClasses = async () => {
+  await fetch(url, options).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+
+      data.forEach((classData) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.setAttribute("data-class-id", classData.id);
+
+        const cardText = document.createElement("p");
+        cardText.innerText = classData.colorLabel;
+
+        card.appendChild(cardText);
+
+        card.style.backgroundColor = classData.color;
+
+        const main = document.querySelector("main");
+        main.appendChild(card);
+        card.addEventListener("click", () => {
+          const classColor = card.getAttribute("data-class-id");
+          window.location.href = `class/?classId=${classColor}`;
+        });
+      });
+    });
   });
-});
+};
+
+getClasses();
