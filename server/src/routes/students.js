c7f59a96b1c3b4fc144608ad78c7d4a8921f8746
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-14 14:05:55 +0100

    made query for students based on classId

--- server/src/routes/students.js
+++ server/src/routes/students.js
@@ -1,66 +1,70 @@
 import express from "express";
 import { db } from "../firebase.js";
 import {
-    addDoc,
-    collection,
-    doc,
-    deleteDoc,
-    getDocs,
+  addDoc,
+  collection,
+  doc,
+  deleteDoc,
+  getDocs,
+  query,
+  where,
 } from "firebase/firestore";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const studentsDocs = await getDocs(collection(db, "students"));
-        const students = studentsDocs.docs.map(doc => doc.data());
-        res.status(200).send(students);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send("Fejl ved hentning af elever");
-    }
+  try {
+    const studentsDocs = await getDocs(collection(db, "students"));
+    const students = studentsDocs.docs.map((doc) => doc.data());
+    res.status(200).send(students);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Fejl ved hentning af elever");
+  }
 });
 
 /* Hent elever i en klasse */
 // TODO: classId er ikke en attribut på student endnu
 router.get("/:classId", async (req, res) => {
-    const classId = req.params.classId;
-    try {
-        const studentsDocs = await getDocs(
-            collection(db, "students"),
-            where("classId", "==", classId)
-        );
-        const students = studentsDocs.docs.map(doc => doc.data());
-        res.status(200).send(students);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send("Fejl ved hentning af elever");
-    }
+  const classId = req.params.classId;
+  console.log(classId);
+  try {
+    const firebaseQuery = query(
+      collection(db, "students"),
+      where("classId", "==", classId)
+    );
+    const studentsDocs = await getDocs(firebaseQuery);
+    const students = studentsDocs.docs.map((doc) => doc.data());
+    res.status(200).send(students);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Fejl ved hentning af elever");
+  }
 });
 
 /* Opret elev */
 router.post("/", async (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    try {
-        const doc = await addDoc(collection(db, "students"), req.body);
-        res.status(201).send("Elev oprettet");
-    } catch (error) {
-        console.log(error);
-        res.status(400).send("Fejl ved oprettelse af elev");
-    }
+  try {
+    const doc = await addDoc(collection(db, "students"), req.body);
+    res.status(201).send("Elev oprettet");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Fejl ved oprettelse af elev");
+  }
 });
 
 /* Slet elev */
 router.delete("/:id", async (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    try {
-        const docDelete = await deleteDoc(doc(db, "students", id));
-        res.status(200).send("Elev slettet");
-    } catch (error) {
-        console.log(error);
-        res.status(404).send("Fejl - eleven findes ikke.");
-    }
+  try {
+    const docDelete = await deleteDoc(doc(db, "students", id));
+    res.status(200).send("Elev slettet");
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Fejl - eleven findes ikke.");
+  }
 });
 
 export default router;
