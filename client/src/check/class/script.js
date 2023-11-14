ad9baa10c92a3f7ec7ecc1c585a3cd8efe8c3fac
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-14 14:05:31 +0100

    implementeret kobling til server api

--- client/src/check/class/script.js
+++ client/src/check/class/script.js
@@ -1,19 +1,76 @@
-const cards = document.querySelectorAll(".card");
+const url = "http://localhost:6969/students";
 
-cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    if (card.classList.contains("checked-in")) {
-      card.classList.remove("checked-in");
-      card.classList.add("checked-out");
-    } else {
-      card.classList.add("checked-in");
-      card.classList.remove("checked-out");
-    }
+const options = {
+  method: "GET",
+};
+
+const fetchData = async (classId) => {
+  await fetch(`${url}/${classId}`, options).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+
+      data.forEach((student) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.setAttribute("data-student-id", student.id);
+
+        const cardImg = document.createElement("img");
+        cardImg.classList.add("avatar");
+        cardImg.src = student.imgUrl
+          ? student.imgUrl
+          : "../../../assets/avatar-placeholder.png";
+
+        const textContainer = document.createElement("div");
+        textContainer.classList.add("text-container");
+
+        const cardName = document.createElement("p");
+        cardName.classList.add("name");
+        cardName.textContent = student.name;
+        textContainer.appendChild(cardName);
+
+        const cardLocation = document.createElement("p");
+        cardLocation.classList.add("location");
+        cardLocation.textContent = student.location
+          ? student.location
+          : "Ukendt lokation";
+        textContainer.appendChild(cardLocation);
+
+        const cardStatus = document.createElement("p");
+        cardStatus.classList.add("status");
+        let checkedIn = student.checkedIn;
+        cardStatus.textContent = checkedIn ? "Ikke hentet" : "Hentet";
+        card.classList.add(checkedIn ? "checked-in" : "checked-out");
+        textContainer.appendChild(cardStatus);
+
+        card.appendChild(cardImg);
+        card.appendChild(textContainer);
+
+        const main = document.querySelector("main");
+        main.appendChild(card);
+
+        card.addEventListener("click", () => {
+          const status = card.querySelector(".status");
+          if (card.classList.contains("checked-in")) {
+            card.classList.remove("checked-in");
+            card.classList.add("checked-out");
+            status.innerHTML = "Hentet";
+          } else {
+            card.classList.add("checked-in");
+            card.classList.remove("checked-out");
+            status.innerHTML = "Ikke hentet";
+          }
+        });
+      });
+    });
   });
-});
+};
 
 const backBtn = document.querySelector(".back-btn");
 
 backBtn.addEventListener("click", () => {
   window.location.href = "../";
 });
+
+const params = new URLSearchParams(window.location.search);
+const classId = params.get("classId");
+fetchData(classId);
