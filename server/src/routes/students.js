a3422c67acbbd5001be8164d99c51156235dc592
Jens Laugaard Pedersen <eaajenlp@students.eaaa.dk>
2023-11-14 10:42:13 +0100

    Ændringer i firebase og student

--- server/src/routes/students.js
+++ server/src/routes/students.js
@@ -1,16 +1,21 @@
 import express, { response } from "express";
+import db from "firebase.js";
+import { addDoc, collection } from "firebase/firestore";
 const router = express.Router();
 
-let students = [];
+
 
 router.post("/", (req, res) => {
     console.log(req.body);
-    students.push(req.body);
+
+    try {
+        addDoc(collection(db, "students"), req.body);
+    } catch (error) {
+        console.log("Fejl");
+    }
     res.status(201);
     res.send("Elev oprettet");
 });
-
-
 
 
 export default router;
