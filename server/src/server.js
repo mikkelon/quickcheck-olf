f9bdccad6e093766a121f1cc9af98f645e2c66f7
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-14 13:28:27 +0100

    installed and setup CORS

--- server/src/server.js
+++ server/src/server.js
@@ -3,13 +3,15 @@ import express from "express";
 import auth from "./routes/auth.js";
 import students from "./routes/students.js";
 import classes from "./routes/classes.js";
+import cors from "cors";
 
 const app = express();
 const port = 6969;
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.use("/auth", auth);
@@ -17,5 +19,5 @@ app.use("/students", students);
 app.use("/classes", classes);
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}...`);
+  console.log(`Server listening on port ${port}...`);
 });
