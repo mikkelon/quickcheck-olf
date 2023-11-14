edf67801ec39d5282db0207509c49e409f1015f6
Jens Laugaard Pedersen <eaajenlp@students.eaaa.dk>
2023-11-14 09:48:38 +0100

    Ændringer i students og server.

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,11 @@
 import express from "express";
 import users from "./routes/users.js";
 import auth from "./routes/auth.js";
+import students from "./routes/students.js";
 
 const app = express();
 const port = 6969;
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -12,7 +14,11 @@ app.get("/", (req, res) => {
 
 app.use("/users", users);
 app.use("/auth", auth);
+app.use("/students", students)
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
 });
+
+
