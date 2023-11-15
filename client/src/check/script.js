7730811ea654bcda3d993021c80b00f7964a3a1b
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-15 11:24:43 +0100

    refactored "getClasses()" into datahandler.js

--- client/src/check/script.js
+++ client/src/check/script.js
@@ -1,34 +1,28 @@
-const url = "http://localhost:6969/classes";
-const options = {
-  method: "GET",
-};
+import { getClasses } from "../datahandler.js";
 
-const getClasses = async () => {
-  await fetch(url, options).then((response) => {
-    response.json().then((data) => {
-      console.log(data);
+const renderCards = async () => {
+  const data = await getClasses();
+  console.log(data);
 
-      data.forEach((classData) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.setAttribute("data-class-id", classData.id);
+  data.forEach((classData) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("data-class-id", classData.id);
 
-        const cardText = document.createElement("p");
-        cardText.innerText = classData.colorLabel;
+    const cardText = document.createElement("p");
+    cardText.innerText = classData.colorLabel;
 
-        card.appendChild(cardText);
+    card.appendChild(cardText);
 
-        card.style.backgroundColor = classData.color;
+    card.style.backgroundColor = classData.color;
 
-        const main = document.querySelector("main");
-        main.appendChild(card);
-        card.addEventListener("click", () => {
-          const classColor = card.getAttribute("data-class-id");
-          window.location.href = `class/?classId=${classColor}`;
-        });
-      });
+    const main = document.querySelector("main");
+    main.appendChild(card);
+    card.addEventListener("click", () => {
+      const classColor = card.getAttribute("data-class-id");
+      window.location.href = `class/?classId=${classColor}`;
     });
   });
 };
 
-getClasses();
+renderCards();
