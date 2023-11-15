4b00b623fd8f97fd652e8d36ddf739c97309d314
gustavh <gkhaavik@gmail.com>
2023-11-15 11:08:54 +0100

    javascript struggles

--- client/src/createParent/javascript/app.js
+++ client/src/createParent/javascript/app.js
@@ -1,14 +1,33 @@
-const MAX_PARENTS = 4;
-const parents = [];
+export const parents = [
+    // {
+    //     id: 1,
+    //     name: 'Parent 1',
+    //     phone: '1234567890',
+    //     email: 'parent@email.com'
+    // }
+];
+export const children = [];
 
 export function addParent(parent) {
-    if (!canCreateParent) {
-        return;
-    }
-    
     parents.push(parent);
 }
 
-export function canCreateParent() {
-    return parents.length < MAX_PARENTS;
+export function addChild(child) {
+    children.push(child);
+}
+
+export function removeParent(parent) {
+    const index = parents.indexOf(parent);
+
+    if (index > -1) {
+        parents.splice(index, 1);
+    }
+}
+
+export function removeChild(child) {
+    const index = children.indexOf(child);
+
+    if (index > -1) {
+        children.splice(index, 1);
+    }
 }
--- client/src/createParent/javascript/gui.js
+++ client/src/createParent/javascript/gui.js
@@ -1,15 +1,19 @@
+import { addParent, parents } from "./app.js";
+
 //const createBtnContainer = document.getElementById("createBtn");
 const parentsContainer = document.getElementById("parents");
 const childrenContainer = document.getElementById("children");
 
+// Components
 var parentsFile = "components/parent.html";
+var childrenFile = "components/child.html";
+
 var createParentButtonFile = "components/createParentButton.html";
 var createChildButtonFile = "components/createChildButton.html";
 
-var childrenFile = "components/child.html";
-
-var parentForms = 1;
 var childForms = 1;
+
+const parser = new DOMParser();
 
 function loadParents() {
   parentsContainer.innerHTML = "";
@@ -17,28 +21,60 @@ function loadParents() {
   fetch(parentsFile)
     .then((response) => response.text())
     .then(function (html) {
-      for (let i = 0; i < parentForms; i++) {
-        parentsContainer.innerHTML += html;
+      for (let i = 0; i < (parents + 1); i++) {
+        const parentElement = document.createElement("div");
+        parentElement.classList.add("parent");
+        parentElement.innerHTML = html;
+
+        // Add an ID to each parent
+        // const parentId = `${createButtonId}-${i + 1}`;
+        parentElement.id = `parent-${i + 1}`;
+
+        parentsContainer.appendChild(parentElement);
       }
     });
 
   fetch(createParentButtonFile)
     .then((response) => response.text())
-    .then(function (html) {
+    .then(function (htmlString) {
+      const html = parser.parseFromString(htmlString, 'text/html');
       createCreateBtn(html, parentsContainer, "createParent", () => {
+        createParent(htmlString);
         loadParents();
-        parentForms++;
-
       });
     });
+}
+
+function createParent(html) {
+  const id = html.parent.getElementById("id").value;
+
+  const name = html.getElementById("name").value;
+  const phone = html.getElementById("phone").value;
+  const email = html.getElementById("email").value;
+
+  const parent = {
+    id: id,
+    name: name,
+    phone: phone,
+    email: email,
+  };
+
+  addParent(parent);
 }
 
 function createCreateBtn(html, containerNode, id, onClick) {
   containerNode.innerHTML += html;
 
+  console.log(containerNode.innerHTML);
+
   const createBtn = document.getElementById(id);
 
   createBtn.addEventListener("click", onClick);
+}
+
+function removeParent(parent) {
+  parent.remove();
+  parentForms--;
 }
 
 function loadChildren() {
@@ -48,7 +84,15 @@ function loadChildren() {
     .then((response) => response.text())
     .then(function (html) {
       for (let i = 0; i < childForms; i++) {
-        childrenContainer.innerHTML += html;
+        const parentElement = document.createElement("div");
+        parentElement.classList.add("child");
+        parentElement.innerHTML = html;
+
+        // Add an ID to each parent
+        // const parentId = `${createButtonId}-${i + 1}`;
+        parentElement.id = `child-${i + 1}`;
+
+        childrenContainer.appendChild(parentElement);
       }
     });
 
