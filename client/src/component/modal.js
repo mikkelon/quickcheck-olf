3857ff72ef3fc72d3c604b943a1e34cc4a7818cf
GustavH <gkhaavik@gmail.com>
2023-11-20 13:37:07 +0100

    hippity hoppity

---
+++ client/src/component/modal.js
@@ -0,0 +1,18 @@
+import { getStudents } from "../datahandler.js";
+
+let child;
+
+async function initGUI() {
+    child = await getStudents().then((students) => {
+        return students[0];
+    })
+
+    console.log(child);
+}
+
+document.addEventListener("DOMContentLoaded", initGUI);
+
+const goBack = document.getElementById("back-icon");
+goBack.addEventListener("click", () => {
+    window.location.href = "../../dashboard/index.html";
+});
