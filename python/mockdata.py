41f2801237f6d0cf45d32a7a5fd61984caf3f2bb
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-20 10:26:56 +0100

    fixed utf-8 encoding issue

--- python/mockdata.py
+++ python/mockdata.py
@@ -5,14 +5,17 @@ import requests
 express_url = "http://localhost:6969"
 
 # Load data from a JSON file
-with open("parents-students.json", "r") as json_file:
+with open("parents-students.json", "r", encoding="utf-8") as json_file:
     data = json.load(json_file)
 
 # Assuming your JSON file contains an array of objects with a structure similar to the one in your Express code
 # You can iterate over the objects and make POST requests
 for obj in data:
     try:
-        response = requests.post(f"{express_url}/students", json=obj)
+        # Serialize the object to JSON and encode it as UTF-8
+        json_data = json.dumps(obj, ensure_ascii=False).encode("utf-8")
+
+        response = requests.post(f"{express_url}/students", data=json_data, headers={"Content-Type": "application/json; charset=utf-8"})
         if response.status_code == 201:
             print(f"Successfully added object: {obj}")
         else:
