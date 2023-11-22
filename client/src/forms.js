4e7576371c8f384d217441742eb24950957842e3
GustavH <gkhaavik@gmail.com>
2023-11-22 13:35:18 +0100

    forms framework og elev redigering

---
+++ client/src/forms.js
@@ -0,0 +1,103 @@
+function createFormsContainer(formElements, onSubmit) {
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("forms-container");
+
+    // const form = document.createElement("form");
+    // form.classList.add("form");
+    // onSubmit && form.addEventListener("submit", onSubmit);
+
+    formElements.forEach((formElement) => {
+        formContainer.appendChild(formElement);
+    });
+
+    return formContainer;
+}
+
+function createFormElement(labelText, inputType, inputId, inputValue, onChange) {
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("form-container");
+    
+    const label = document.createElement("label");
+    label.classList.add("forms-label");
+    label.textContent = labelText;
+    
+    const input = document.createElement("input");
+    input.classList.add("forms-input");
+    input.setAttribute("type", inputType);
+    input.setAttribute("id", inputId);
+    input.value = inputValue;
+    
+    formContainer.appendChild(label);
+    formContainer.appendChild(input);
+    
+    onChange && input.addEventListener("change", onChange);
+    
+    return formContainer;
+}
+
+function createDropdownFormElement(
+    labelText,
+    inputId,
+    options,
+    optionData,
+    selected
+) {
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("form-container");
+
+    const label = document.createElement("label");
+    label.classList.add("forms-label");
+    label.textContent = labelText;
+
+    let select = document.createElement("select");
+    select.classList.add("forms-input");
+    select.setAttribute("id", inputId);
+
+    options.forEach((option) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = option.colorLabel;
+        optionElement.textContent = option.colorLabel;
+
+        optionElement.setAttribute(optionData, option.id);
+
+        select.appendChild(optionElement);
+    });
+
+    const selectedIndex = options.findIndex(
+        (option) => option.id === selected
+    );
+    select.selectedIndex = selectedIndex;
+
+    formContainer.appendChild(label);
+    formContainer.appendChild(select);
+
+    return formContainer;
+}
+
+function createTextButton(id, text, onClick) {
+    const button = document.createElement("button");
+    button.classList.add("forms-button");
+    button.setAttribute("id", id);
+    button.innerHTML = text ?? "";
+    button.addEventListener("click", onClick);
+
+    return button;
+}
+
+function createIconButton(id, icon, onClick, bgColor) {
+    const button = document.createElement("button");
+    button.setAttribute("id", id);
+    button.innerHTML = icon ?? "";
+    button.addEventListener("click", onClick);
+    button.style.backgroundColor = bgColor ?? "";
+
+    return button;
+}
+
+export {
+    createFormsContainer,
+    createFormElement,
+    createDropdownFormElement,
+    createTextButton,
+    createIconButton
+};
--- client/src/web/dashboard/student-overview/component/gui.js
+++ client/src/web/dashboard/student-overview/component/gui.js
@@ -1,35 +1,98 @@
 import { getParentsById } from "../../../../datahandler.js";
 import { createParent, deleteParent, getAllParents } from "./crud.js";
-
-let child;
+import { createFormElement, createFormsContainer, createIconButton, createTextButton } from "../../../../forms.js";
 
 const checkMark = "/client/assets/icons/check.svg";
 const crossMark = "/client/assets/icons/cross.svg";
 
-const avatar = document.getElementById("avatar");
-const nameHeader = document.getElementById("name-header");
-const birthday = document.getElementById("birthday");
-const clazz = document.getElementById("class");
+let editing = false;
+
+const child = {
+    "checkedIn": false,
+    "name": "Mads Jensen",
+    "parentsId": "jqzQiXeNiOcZMdZcTNwN",
+    "birthday": "2005-02-15",
+    "classId": "0777Ja1ckgt5F6XcaxZR",
+    "class": {
+        "id": "0777Ja1ckgt5F6XcaxZR",
+        "class": 2,
+        "colorLabel": "Rød",
+        "color": "#FF4D4D"
+    }
+};
 
 async function initGUI() {
-    const objStr = localStorage.getItem("child");
-    child = JSON.parse(objStr);
-
-    console.log(child);
+    // const objStr = localStorage.getItem("child");
+    // child = JSON.parse(objStr);
+    const avatar = document.getElementById("avatar");
+    const nameHeader = document.getElementById("name-header");
+    const birthdayElement = document.getElementById("birthday");
+    const clazzElement = document.getElementById("class");
 
     nameHeader.innerHTML = child.name;
-    birthday.value = child.birthday;
-    clazz.value = child.class.colorLabel;
+
+    birthdayElement.value = child.birthday;
+    clazzElement.value = child.class.colorLabel;
 
     setStatus(child.checkedIn);
 
-    await getParentsById(child.parents).then((res) => {
+    await getParentsById(child.parentsId).then((res) => {
         res.parents.map((parent) => {
             createParent(parent.name, parent.phone, parent.email);
         });
-    }
-    );
+    });
+
     setParents();
+    setState(false);
+
+    const closeBtn = document.getElementById("closeBtn")
+    closeBtn.addEventListener("click", () => {
+        noteModal.style.display = "none";
+    });
+}
+
+function addButtons() {
+    const customizeButtons = document.getElementById("header-right");
+    customizeButtons.innerHTML = "";
+
+    if (editing) {
+        const saveBtn = createTextButton("save", "Gem", () => {
+            editing = !editing;
+            setState(editing);
+        });
+
+        const cancelbtn = createTextButton("cancel", "Annuller", () => {
+            editing = !editing;
+            setState(editing);
+        });
+
+        customizeButtons.appendChild(cancelbtn);
+        customizeButtons.appendChild(saveBtn);
+    } else {
+        const deleteBtn = createTextButton("delete", "Slet", () => {
+            console.log("Delete button clicked");
+        });
+
+        const editBtn = createTextButton("edit", "Rediger", () => {
+            editing = !editing;
+            setState(editing);
+        });
+
+        customizeButtons.appendChild(deleteBtn);
+        customizeButtons.appendChild(editBtn);
+    }
+}
+
+function setState(editing) {
+    const forms = document.querySelectorAll(".forms-input");
+
+    forms.forEach((form) => {
+        form.disabled = !editing;
+    });
+
+    setParents();
+
+    addButtons();
 }
 
 function setStatus(checkedIn) {
@@ -52,19 +115,21 @@ async function setParents() {
         parentsContainer.appendChild(parentDiv);
     });
 
-    const parentButton = createAddButton(
-        "createParent",
-        "Tilføj Forældre",
-        () => {
-            // Handle the click event for adding a child
-            // You can show a form or perform any other action
-            console.log("Add Parent button clicked");
-            createParent("", "", "");
-            setParents();
-        }
-    );
+    if (editing) {
+        const parentButton = createAddButton(
+            "createParent",
+            "Tilføj Forældre",
+            () => {
+                // Handle the click event for adding a child
+                // You can show a form or perform any other action
+                console.log("Add Parent button clicked");
+                createParent("", "", "");
+                setParents();
+            }
+        );
 
-    parentsContainer.appendChild(parentButton);
+        parentsContainer.appendChild(parentButton);
+    }
 }
 
 // Function to create a button for adding a parent or child
@@ -73,84 +138,19 @@ function createAddButton(id, text, clickHandler) {
     addButton.id = id;
     addButton.classList.add("container", "createBtn");
 
-    addButton.innerHTML = `
-      <svg class ="add" width="48px" height="48px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-          d="M12.75 9C12.75 8.58579 12.4142 8.25 12 8.25C11.5858 8.25 11.25 8.58579 11.25 9L11.25 11.25H9C8.58579 11.25 8.25 11.5858 8.25 12C8.25 12.4142 8.58579 12.75 9 12.75H11.25V15C11.25 15.4142 11.5858 15.75 12 15.75C12.4142 15.75 12.75 15.4142 12.75 15L12.75 12.75H15C15.4142 12.75 15.75 12.4142 15.75 12C15.75 11.5858 15.4142 11.25 15 11.25H12.75V9Z"
-          fill="#56E87F" />
-      <path fill-rule="evenodd" clip-rule="evenodd"
-          d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z"
-          fill="#56E87F" />
-  </svg>
-          <p>${text}</p>`;
+    addButton.innerHTML += `<svg class ="add" width="48px" height="48px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M12.75 9C12.75 8.58579 12.4142 8.25 12 8.25C11.5858 8.25 11.25 8.58579 11.25 9L11.25 11.25H9C8.58579 11.25 8.25 11.5858 8.25 12C8.25 12.4142 8.58579 12.75 9 12.75H11.25V15C11.25 15.4142 11.5858 15.75 12 15.75C12.4142 15.75 12.75 15.4142 12.75 15L12.75 12.75H15C15.4142 12.75 15.75 12.4142 15.75 12C15.75 11.5858 15.4142 11.25 15 11.25H12.75V9Z"
+        fill="#56E87F" />
+    <path fill-rule="evenodd" clip-rule="evenodd"
+        d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z"
+        fill="#56E87F" />
+</svg>`;
+    addButton.innerHTML += `<p">${text}</p>`;
 
     addButton.addEventListener("click", clickHandler);
 
     return addButton;
-}
-
-// Function to create a form element
-function createFormElement(labelText, inputType, inputId, inputValue) {
-    const formContainer = document.createElement("div");
-    formContainer.classList.add("form-container");
-
-    const label = document.createElement("label");
-    label.classList.add("forms-label");
-    label.textContent = labelText;
-
-    const input = document.createElement("input");
-    input.classList.add("forms-input");
-    input.setAttribute("type", inputType);
-    input.setAttribute("id", inputId);
-    input.value = inputValue;
-
-    formContainer.appendChild(label);
-    formContainer.appendChild(input);
-
-    return formContainer;
-}
-
-// Function to create a dropdown form element
-function createDropdownFormElement(
-    labelText,
-    inputId,
-    options,
-    selectedClassId
-) {
-    console.log("Creating dropdown form element");
-    console.log("Selected classID:", selectedClassId);
-
-    const formContainer = document.createElement("div");
-    formContainer.classList.add("form-container");
-
-    const label = document.createElement("label");
-    label.classList.add("forms-label");
-    label.textContent = labelText;
-
-    let select = document.createElement("select");
-    select.classList.add("forms-input");
-    select.setAttribute("id", inputId);
-
-    options.forEach((option) => {
-        const optionElement = document.createElement("option");
-        optionElement.value = option.colorLabel;
-        optionElement.textContent = option.colorLabel;
-
-        optionElement.setAttribute("data-class-id", option.id);
-
-        select.appendChild(optionElement);
-    });
-
-    // Find index of option where data-class-id matches selectedClassId
-    const selectedIndex = options.findIndex(
-        (option) => option.id === selectedClassId
-    );
-    select.selectedIndex = selectedIndex;
-
-    formContainer.appendChild(label);
-    formContainer.appendChild(select);
-
-    return formContainer;
 }
 
 // Function to create a parent element
@@ -159,47 +159,28 @@ function createParentElement(parent, index) {
     parentDiv.classList.add("container");
 
     const headerDiv = document.createElement("div");
-    headerDiv.classList.add("container-header");
-    headerDiv.innerHTML = `<h2>Forælder</h2>
-              <svg class="delete" width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                      d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                      stroke="#FF5656" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>`;
-    headerDiv
-        .querySelector(".delete")
-        .addEventListener("click", () => deleteParentHandler(index));
+    if (editing) {
+        headerDiv.classList.add("container-header");
+        const deleteIcon = createIconButton("delete", "<i class='fas fa-trash'></i>", () => {
+            deleteParentHandler(index);
+        });
+        headerDiv.appendChild(deleteIcon);
+    }
 
-    const formsContainer = document.createElement("div");
-    formsContainer.classList.add("forms-container");
+    const nameForm = createFormElement("Fulde navn", "text", "name", parent.name, () => {
+        updateParentData(index, "name", nameInput.value);
+    });
 
-    const nameForm = createFormElement("Fulde navn", "text", "name", parent.name);
-    formsContainer.appendChild(nameForm);
-
-    const phoneForm = createFormElement(
-        "Telefonnummer",
-        "text",
-        "phone",
-        parent.phone
+    const phoneForm = createFormElement("Telefonnummer", "text", "phone", parent.phone, () => {
+        updateParentData(index, "phone", phoneInput.value);
+    }
     );
-    formsContainer.appendChild(phoneForm);
 
-    const emailForm = createFormElement("E-mail", "text", "email", parent.email);
-    formsContainer.appendChild(emailForm);
+    const emailForm = createFormElement("E-mail", "text", "email", parent.email, () => {
+        updateParentData(index, "email", emailInput.value);
+    });
 
-    const nameInput = nameForm.querySelector(".forms-input");
-    const phoneInput = phoneForm.querySelector(".forms-input");
-    const emailInput = emailForm.querySelector(".forms-input");
-
-    nameInput.addEventListener("blur", () =>
-        updateParentData(index, "name", nameInput.value)
-    );
-    phoneInput.addEventListener("blur", () =>
-        updateParentData(index, "phone", phoneInput.value)
-    );
-    emailInput.addEventListener("blur", () =>
-        updateParentData(index, "email", emailInput.value)
-    );
+    const formsContainer = createFormsContainer([nameForm, phoneForm, emailForm]);
 
     parentDiv.appendChild(headerDiv);
     parentDiv.appendChild(formsContainer);
@@ -227,9 +208,6 @@ checkInOutBtn.addEventListener("click", () => {
     setStatus(child.checkedIn);
 });
 
-document.addEventListener("DOMContentLoaded", initGUI);
-
-
 const noteModal = document.getElementById("note-modal");
 
 const addNote = document.querySelector(".add-note-btn");
@@ -237,10 +215,4 @@ addNote.addEventListener("click", () => {
     noteModal.style.display = "block";
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const closeBtn = document.getElementById("closeBtn")
-
-    closeBtn.addEventListener("click", () => {
-        noteModal.style.display = "none";
-    });
-});
+document.addEventListener("DOMContentLoaded", initGUI);
