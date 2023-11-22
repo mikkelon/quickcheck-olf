3931e2666542ef071ec2273529e6afee0e9f65bf
AndersMikkelsen <ande077h@gmail.com>
2023-11-22 13:55:49 +0100

    update to tabletview

--- client/src/tablet/script.js
+++ client/src/tablet/script.js
@@ -1,4 +1,8 @@
-import { getClasses, getStudentsByClassId } from "../datahandler.js";
+import {
+  getClasses,
+  getStudentsByClassId,
+  getStudents,
+} from "../datahandler.js";
 
 const renderCards = async () => {
   const classesData = await getClasses();
@@ -6,42 +10,42 @@ const renderCards = async () => {
 
   classesData.sort((a, b) => a.class - b.class);
 
-  for (const classData of classesData) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute("data-class-id", classData.id);
+  try {
+    const allStudents = await getStudents();
 
-    const cardText = document.createElement("p");
-    cardText.innerText = `${classData.colorLabel}`;
+    for (const classData of classesData) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.setAttribute("data-class-id", classData.id);
 
-    card.appendChild(cardText);
+      const cardText = document.createElement("p");
+      cardText.innerText = `${classData.colorLabel}`;
 
-    card.style.backgroundColor = classData.color;
+      card.appendChild(cardText);
 
-    const section = document.querySelector("section");
-    section.appendChild(card);
+      card.style.backgroundColor = classData.color;
 
-    // Henter alle elever i klassen
-    try {
-      const students = await getStudentsByClassId(classData.id);
+      const section = document.querySelector("section");
+      section.appendChild(card);
+
+      // Henter alle elever i klassen
+
+      const students = allStudents.filter(students => students.classId === classData.id);
       const checkedInStudents = students.filter((student) => student.checkedIn);
       const totalStudents = document.createElement("div");
       totalStudents.innerText = `Tjekket ind: ${checkedInStudents.length}/${students.length}`;
 
       // Opdaterer kortet med hvor mange der er tjekket ind
       card.appendChild(totalStudents);
-    } catch (error) {
-      console.error(
-        `Error fetching students for class ${classData.id}:`,
-        error
-      );
-      cardText.innerText = `${classData.colorLabel} - Checked In: Error`;
-    }
 
-    card.addEventListener("click", () => {
-      const classColor = card.getAttribute("data-class-id");
-      window.location.href = `class/?classId=${classColor}`;
-    });
+      card.addEventListener("click", () => {
+        const classColor = card.getAttribute("data-class-id");
+        window.location.href = `class/?classId=${classColor}`;
+      });
+    }
+  } catch (error) {
+    console.error(`Der skete en fejl ved indlæsning af elever:`, error);
+    cardText.innerText = `Checked In: Error`;
   }
 };
 
