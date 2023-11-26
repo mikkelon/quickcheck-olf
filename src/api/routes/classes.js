0ff382d09bce70be66ebd1fcaa2f306ae0480507
Mads <mads.soenderstrup@gmail.com>
2023-11-26 21:35:26 +0100

    refactored classes to use controller

--- src/api/routes/classes.js
+++ src/api/routes/classes.js
@@ -1,24 +1,14 @@
 import express from "express";
-import { db } from "../../config/firebase.js";
 import {
-  collection,
-  doc,
-  getDoc,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
+  getClasses,
+  getClassById,
+  getStudentsByClassId,
+} from "../controllers/classesController.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const classesDocs = await getDocs(collection(db, "classes"));
-    const classes = classesDocs.docs.map(doc => {
-      return {
-        id: doc.id,
-        ...doc.data(),
-      };
-    });
+    const classes = await getClasses();
     res.status(200).send(classes);
   } catch (error) {
     console.log(error);
@@ -29,9 +19,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const docRef = doc(db, "classes", id);
-    const docSnap = await getDoc(docRef);
-    const clazz = docSnap.data();
+    const clazz = await getClassById(id);
     res.status(200).send(clazz);
   } catch (error) {
     console.log(error);
@@ -39,19 +27,10 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-/* Hent elever i en klasse */
-// TODO: classId er ikke en attribut på student endnu
 router.get("/:classId/students", async (req, res) => {
   const classId = req.params.classId;
   try {
-    const firebaseQuery = query(
-      collection(db, "students"),
-      where("classId", "==", classId)
-    );
-    const studentsDocs = await getDocs(firebaseQuery);
-    const students = studentsDocs.docs.map(doc => {
-      return { id: doc.id, ...doc.data() };
-    });
+    const students = await getStudentsByClassId(classId);
     res.status(200).send(students);
   } catch (error) {
     console.log(error);
