aa0186450c4d094e2bdf40edc999c9c79180aa31
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-27 14:24:10 +0100

    fixed firebase client SDK setup

--- src/public/utility/firebase.js
+++ src/public/utility/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCJYnP1ZAcCoYD6_b_sfHRmqBEiB7wXlgc",
@@ -9,8 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export { db, auth };
+export { auth };
