f015bc8d8d5ac2f084244d1aef3de3cd56effa1b
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-11-28 13:01:38 +0100

    now setting custom user claims when creating a user

--- src/api/routes/signup.js
+++ src/api/routes/signup.js
@@ -6,9 +6,32 @@ router.post("/employee", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const userRecord = await createUser(email, password);
+    const userRecord = await createUser(email, password, "employee");
 
     res.status(200).send({
+      message: "Successfully created new user",
+      data: {
+        uid: userRecord.uid,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating new user:", error);
+    res.status(500).send({
+      message: "Error creating new user",
+      data: {
+        error: error.message, // Sending only the error message for better client-side handling
+      },
+    });
+  }
+});
+
+router.post("/dev", async (req, res) => {
+  const { email, password, role } = req.body;
+
+  try {
+    const userRecord = await createUser(email, password, role);
+
+    res.status(201).send({
       message: "Successfully created new user",
       data: {
         uid: userRecord.uid,
