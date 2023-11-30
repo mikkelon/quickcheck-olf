994f1186e75aabc5784ce54d33c0f48b72999c40
mads-sj <mads.soenderstrup@gmail.com>
2023-11-30 09:51:30 +0100

    finished story 22

--- src/public/utility/realtime.js
+++ src/public/utility/realtime.js
@@ -1,52 +1,53 @@
-import { ref, onChildAdded, onChildChanged, onChildRemoved, set, push } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+import {
+  ref,
+  onChildAdded,
+  onChildChanged,
+  onChildRemoved,
+  set,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 import { realtimeDB } from "./firebase.js";
-
 
 const realtime = ref(realtimeDB, "notice-board");
 
-export const realtimeNoticeBoard = (callback) => {
-    onChildAdded(realtime, (snapshot) => {
-        callback(snapshot.val());
-    });
-}
+export const realtimeNoticeBoard = callback => {
+  onChildAdded(realtime, snapshot => {
+    callback(snapshot.val());
+  });
+};
 
-export const realtimeNoticeBoardUpdate = (callback) => {
-    onChildChanged(realtime, (snapshot) => {
-        callback(snapshot.val());
-    });
-}
+export const realtimeNoticeBoardUpdate = callback => {
+  onChildChanged(realtime, snapshot => {
+    callback(snapshot.val());
+  });
+};
 
-export const realtimeNoticeBoardDelete = (callback) => {
-    onChildRemoved(realtime, (snapshot) => {
-        callback(snapshot.val());
-    });
-}
+export const realtimeNoticeBoardDelete = callback => {
+  onChildRemoved(realtime, snapshot => {
+    callback(snapshot.val());
+  });
+};
 
 /**
- * 
- * @param {{name, relation}} sender 
- * @param {[{name, class}, {name, class}]} concerns 
- * @param {string} message 
+ *
+ * @param {{name, relation}} sender
+ * @param {[{name, class}, {name, class}]} concerns
+ * @param {string} message
  */
-export function writeNotice(data) {
-    const { sendDate, sender, concerns, message } = data;
-    console.log(sender)
+export async function writeNotice(data) {
+  const { sendDate, sender, concerns, message } = data;
+  console.log(sender);
 
-    console.log(concerns)
+  console.log(concerns);
 
-    console.log()
+  console.log();
 
-    const newPostRef = push(realtime);
-    set(newPostRef, {
-        sender: sender,
-        concerns: concerns,
-        message: message,
-        read: false,
-        sendDate: sendDate
-    }).then(() => {
-        console.log('Notice written to realtime database');
-    }).catch((error) => {
-        console.error('Error writing notice to realtime database:', error);
-    });
-
+  const newPostRef = push(realtime);
+  await set(newPostRef, {
+    sender: sender,
+    concerns: concerns,
+    message: message,
+    read: false,
+    sendDate: sendDate,
+  });
 }
